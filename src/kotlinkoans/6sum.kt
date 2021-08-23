582be567fea1b9877a455377b91d0659cf7da6d2
raisara <49322828+raisara@users.noreply.github.com>
2021-08-23 16:13:06 +0400

    Update 6sum.kt

--- src/kotlinkoans/6sum.kt
+++ src/kotlinkoans/6sum.kt
@@ -1,2 +1,5 @@
 package kotlinkoans
 
+// Return the sum of prices for all the products ordered by a given customer
+fun moneySpentBy(customer: Customer): Double =
+        customer.orders.flatMap(Order::products).sumByDouble { it.price }
