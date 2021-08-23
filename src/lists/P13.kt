10e96ca5d1a0c2459417b679be435ca832fa8def
raisara <49322828+raisara@users.noreply.github.com>
2021-08-23 17:14:02 +0400

    Update P13.kt

--- src/lists/P13.kt
+++ src/lists/P13.kt
@@ -1,9 +1,22 @@
-//package lists
-//
-//fun <T> encodeDirect(list: List<T?>): List<Pair<Int, T>>{
-//
-//}
-//
-//fun main(){
-//    println(encodeDirect("aaaabccaadeeee".toList()))
-//}
+package lists
+
+fun <T> encodeDirect(list: List<T?>): List<Pair<Int, T?>>{
+    val myList = mutableListOf<Pair<Int, T?>>()
+    var element: T? = list[0]
+    var count = 1
+    for(i in 0..list.size-2){
+        if(list[i]==list[i+1]){
+            count++
+        }else{
+            myList.add(count to element)
+            element=list[i+1]
+            count = 1
+        }
+    }
+    myList.add(count to element)
+    return myList
+}
+
+fun main(){
+    println(encodeDirect("aaaabccaadeeee".toList()))
+}
