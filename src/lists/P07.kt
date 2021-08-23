16e848e9d5f2d75ffb0cd8a198f53fca0202cc9c
raisara <49322828+raisara@users.noreply.github.com>
2021-08-23 17:49:32 +0400

    Update P07.kt

--- src/lists/P07.kt
+++ src/lists/P07.kt
@@ -1,10 +1,11 @@
-//package lists
-//
-//fun flatten(list: List<Any>): List<Any>{
-//    list.flatMap{it}
-//    return list
-//}
-//
-//fun main(){
-//    println(flatten(listOf(listOf(1, 1), 2, listOf(3, listOf(5, 8)))))
-//}
+package lists
+
+fun flatten(list: List<Any>): List<Any> =
+    list.flatMap{
+        if(it is List<*>) flatten(it as List<Any>) else listOf(it)
+    }
+
+
+fun main(){
+    println(flatten(listOf(listOf(1, 1), 2, listOf(3, listOf(5, 8)))))
+}
