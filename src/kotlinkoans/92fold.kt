2564315a7fe7e408cf2ea2580297639e9ccd803b
raisara <49322828+raisara@users.noreply.github.com>
2021-08-23 16:20:14 +0400

    Update 92fold.kt

--- src/kotlinkoans/92fold.kt
+++ src/kotlinkoans/92fold.kt
@@ -1,2 +1,12 @@
 package kotlinkoans
 
+// Return the set of products that were ordered by all customers
+fun Shop.getProductsOrderedByAll(): Set<Product> {
+    val orderedProducts = customers.flatMap{it.getOrderedProducts()}.toSet()
+    return customers.fold(orderedProducts, { orderedByAll, customer ->
+        orderedByAll.intersect(customer.getOrderedProducts())
+    })
+}
+
+fun Customer.getOrderedProducts(): List<Product> =
+    orders.flatMap{it.products}
